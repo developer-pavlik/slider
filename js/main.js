9e308848e0c6930533ea2882712918ad2ead1e6d
Robin <robin>
2015-06-23 16:41:45 +0300

    Второй комит

--- js/main.js
+++ js/main.js
@@ -1,79 +1,129 @@
  $(document).ready(function(){
+  
     var elementCount = $(".items").length;
-    var currentPage = 1;
-    var dX = 0;
+    var workspace =  $(".content").width();
+    var lZone, cZone, rZone;
+    var cDx=0;
+    lZone= 0;
 
-// Если количество элементов в слайдере   четной, то все элементы отображаются одинаковыми. Иначе  первый элемент становится крупным .  
+
+    switch(workspace){
+        case 1420:
+            var Vworkspace = 12;
+            break;
+        case 1180:
+           Vworkspace = 10;
+            break;
+        case 940:
+            Vworkspace = 8;
+            break;
+    }
+
 
     if(elementCount%2 == 1){
         $(".items:first").addClass("big");
-        var wrapperWidth = (elementCount - 1)/2*240+480;
+        var vItems = elementCount+3;
     }else{
-        var wrapperWidth = elementCount/2*240;
+        vItems = elementCount;
     }
-    
-    
-    var workspace =  $(".content").width()+20;
-    var pageCount = Math.ceil(wrapperWidth/workspace); 
 
-    function btnStatus(){
-        if(pageCount==1){
-            $(".prev").hide();
+    if(Vworkspace > vItems){
+        cZone=vItems;
+        $(".next").hide();
+    }else{
+        cZone=Vworkspace;
+    }
+
+    rZone = vItems - (lZone+cZone);        
+
+
+    console.log(lZone, cZone, rZone);
+    console.log(vItems);
+
+
+
+    function btnStat(){
+         $(".next").show();
+        if(rZone==0){
             $(".next").hide();
         }
-        
-        $(".prev").show();
-        if(currentPage == 1){
+         $(".prev").show();
+        if(lZone==0){
             $(".prev").hide();
         }
-
-        $(".next").show();
-        if(currentPage == pageCount){
-            $(".next").hide();
-        }    
     }
-    
-    btnStatus();
-    
-    var oldSize = $(".content").width()+20;
-    var offset = 0;
-    
-    $(window).on("resize", function(){
-        window.resizeEvt;
-        $(window).resize(function(){
-            clearTimeout(window.resizeEvt);
-            window.resizeEvt = setTimeout(function(){
-                workspace= $(".content").width()+20;
-                pageCount = Math.ceil(wrapperWidth/workspace);  
 
-                offset = workspace-oldSize;
-                console.log(offset);
+    btnStat();
 
-                oldSize=workspace;
-                btnStatus(); 
-            }, 250);
-        });  
-    });
-    
     $(".next").on('click', function(){
-         if(currentPage==1){
-            offset=0;          
+        if(rZone >= cZone){
+            var dx = $(".content").width()+20;
+            cDx+=dx;
+            $(".wrapper").animate({'margin-left': -cDx+"px"}, 1000);
+            lZone+=cZone;
+            rZone-=cZone;
+            console.log(lZone, cZone, rZone);
+        }else{
+            dx=rZone/2*240;
+            cDx+=dx;
+            $(".wrapper").animate({'margin-left': -cDx+"px"}, 1000);
+            lZone+=rZone;
+            rZone-=rZone;
+            console.log(lZone, cZone, rZone);
         }
-        $(".wrapper").animate({'margin-left': dX-workspace+"px"}, 1000);
-        dX-=workspace;
-        currentPage++;
-        btnStatus();
+        btnStat();
+
     })
-    
     $(".prev").on('click', function(){
-        currentPage--;
-        btnStatus();
-        if(currentPage==1){
-             offset=0;          
+        if(lZone >= cZone){
+            var dx = $(".content").width()+20;
+            cDx-=dx;
+            $(".wrapper").animate({'margin-left': -cDx+"px"}, 1000);
+            lZone-=cZone;
+            rZone+=cZone;
+            console.log(lZone, cZone, rZone);
+        }else{
+            dx=lZone/2*240;
+            cDx-=dx;
+            $(".wrapper").animate({'margin-left': -cDx+"px"}, 1000);
+            rZone+=lZone;
+            lZone-=lZone;
+            console.log(lZone, cZone, rZone);
         }
-        $(".wrapper").animate({'margin-left': dX+workspace-offset+"px"}, 1000);
-        dX+=workspace;
+        btnStat();
+
     })
- 
+
+
+    window.resizeEvt;
+    $(window).resize(function(){
+        clearTimeout(window.resizeEvt);
+        window.resizeEvt = setTimeout( function(){
+            workspace =  $(".content").width();
+
+            switch(workspace){
+                case 1420:
+                    var newVworkspace = 12;
+                    break;
+                case 1180:
+                    newVworkspace = 10;
+                    break;
+                case 940:
+                    newVworkspace = 8;
+                    break;
+            }
+
+            var dElemrnt=Vworkspace-newVworkspace;
+            Vworkspace=newVworkspace;
+            rZone+=dElemrnt;
+            cZone-=dElemrnt;
+            console.log(lZone, cZone, rZone);
+
+            //console.log(dElemrnt);
+
+
+        }, 250);
+    });  
+
 });
 
