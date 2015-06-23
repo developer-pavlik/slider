dd1b0c93349ae0d49158b00459442c5de4a65f5a
Robin <robin>
2015-06-23 10:18:54 +0400

    2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
     var elementCount = $(".items").length;
     var currentPage = 1;
     var dX = 0;
- 
+
 // Если количество элементов в слайдере   четной, то все элементы отображаются одинаковыми. Иначе  первый элемент становится крупным .  
 
     if(elementCount%2 == 1){
@@ -15,10 +15,7 @@
     
     var workspace =  $(".content").width()+20;
     var pageCount = Math.ceil(wrapperWidth/workspace); 
-    
-    
-    
-    
+
     function btnStatus(){
         if(pageCount==1){
             $(".prev").hide();
@@ -38,29 +35,45 @@
     
     btnStatus();
     
-    $(window).resize(function(){     
-        workspace= $(".content").width()+20;
-        pageCount = Math.ceil(wrapperWidth/workspace);  
-        $(".wrapper").css({'margin-left': 0+"px"});
-        currentPage = 1;
-        dX = 0;
-        btnStatus(); 
+    var oldSize = $(".content").width()+20;
+    var offset = 0;
+    
+    $(window).on("resize", function(){
+        window.resizeEvt;
+        $(window).resize(function(){
+            clearTimeout(window.resizeEvt);
+            window.resizeEvt = setTimeout(function(){
+                workspace= $(".content").width()+20;
+                pageCount = Math.ceil(wrapperWidth/workspace);  
+
+                offset = workspace-oldSize;
+                console.log(offset);
+
+                oldSize=workspace;
+                btnStatus(); 
+            }, 250);
+        });  
     });
     
     $(".next").on('click', function(){
+         if(currentPage==1){
+            offset=0;          
+        }
+        $(".wrapper").animate({'margin-left': dX-workspace+"px"}, 1000);
+        dX-=workspace;
         currentPage++;
         btnStatus();
-        $(".wrapper").animate({'margin-left': dX-workspace+"px"}, 1000);
-        dX-=workspace;       
     })
     
     $(".prev").on('click', function(){
         currentPage--;
         btnStatus();
-        $(".wrapper").animate({'margin-left': dX+workspace+"px"}, 1000);
+        if(currentPage==1){
+             offset=0;          
+        }
+        $(".wrapper").animate({'margin-left': dX+workspace-offset+"px"}, 1000);
         dX+=workspace;
     })
-    
-    
+ 
 });
 
